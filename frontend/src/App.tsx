a61dc809e7f20c871e57e8a5c9f62e3e95340c28
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-09-10 18:43:17 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,45 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
   return (
     <>
-    <NavBar />
-    <div className="container">
+      <NavBar />
+      <div className="container">
 
-      <h1 className="text-primary">Olá Mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-      <DataTable />
+        <div className="row px-3">
 
-    </div>
-    <Footer />
+          <div className="col-sm-6">
+
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+
+            <BarChart />
+
+          </div>
+
+          <div className="col-sm-6">
+
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+
+            <DonutChart />
+
+          </div>
+
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+
+        <DataTable />
+
+      </div>
+      <Footer />
     </>
   );
 }
