1423bf65f3ecb3179bc2e1d8174aa5cbe9eeb0ac
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-09-12 22:15:47 -0300

    fix

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -10,16 +10,16 @@ type Props = {
 const Pagination = ( { page, onPageChange } : Props ) => {
 
     return(
-        <div className="row d-flex justify-content-center">
+        <div className="d-flex justify-content-center">
     <nav>
         <ul className="pagination">
-            <li className={`page-item ${page.first ? 'disable' : ''} `}>
+            <li className={`page-item ${page.first ? 'disabled' : ''} `}>
                 <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
             </li>
             <li className="page-item disabled">
                 <span className="page-link">{page.number + 1}</span>
             </li>
-            <li className={`page-item ${page.last ? 'disable' : ''} `}>
+            <li className={`page-item ${page.last ? 'disabled' : ''} `}>
                 <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
             </li>
         </ul>
